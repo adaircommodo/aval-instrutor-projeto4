07efec53a02d5c647c672906fabcc8a376d00407
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-18 09:19:17 -0300

    Criação dos atributos solicitados

--- Projeto4/src/projeto4/InteiroSet.java
+++ Projeto4/src/projeto4/InteiroSet.java
@@ -1,5 +1,14 @@
 package projeto4;
 
+import java.util.Arrays;
+
 public class InteiroSet {
+
+    private static final int MIN = 0;
+
+    private static final int MAX = 100;
+
+    private boolean[] a; // [0...100]
+
 
 }
