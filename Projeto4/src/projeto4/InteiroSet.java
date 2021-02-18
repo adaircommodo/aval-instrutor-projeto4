b71147777b987156162b55c5239b717c6cce1f43
Adair José Rossatto Commodo <adaircommodo@gmail.com>
2021-02-18 09:22:28 -0300

    Criação do Construtor pra iniciar o array

--- Projeto4/src/projeto4/InteiroSet.java
+++ Projeto4/src/projeto4/InteiroSet.java
@@ -10,5 +10,14 @@ public class InteiroSet {
 
     private boolean[] a; // [0...100]
 
+   /**
+     O construtor sem argumento 
+     inicializa o array java como conjunto vazio (isto é, um 
+     conjunto cuja representação de array contém todos os 
+     valores false).
+     */
+    public InteiroSet() {
+        a = new boolean[MAX + 1];
+    }
 
 }
